df1a560b417b9186bc192ab91a12a2b0d78c3447
luis <lu.al.bu@hotmail.com>
2020-09-06 19:48:34 -0500

    some minor advances...

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,21 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Main where
 
-import Lib
+import Graphics.Vty
 
-main :: IO ()
-main = someFunc
+welcomeMessage = " :D welcome to renegade,\
+                 \ we know GUI's are too much\
+                 \ and plain terminal isn't any fun "
+
+main = do
+  cfg <- standardIOConfig
+  vty <- mkVty cfg
+  let line0 = string (defAttr ` withForeColor ` white) welcomeMessage 
+      img = line0 -- <-> line1 <-> line0
+      pic = picForImage img
+  update vty pic
+  e <- nextEvent vty
+  print e
+  shutdown vty
+  print ("Last event was: " ++ show e)
+  
